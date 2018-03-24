2259cae7cbfc058b821a4edb06334016a0d726e0
Giovanni Sakti <giosakti@gmail.com>
2018-03-24 19:28:39 +0700

    [gio] generate devise views and allow username to be used

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,8 @@ class User < ApplicationRecord
     :registerable,
     :recoverable, 
     :rememberable, 
-    :trackable
+    :trackable,
+    authentication_keys: [:login]
   validates_confirmation_of :password
 
   #
@@ -51,10 +52,12 @@ class User < ApplicationRecord
   #
 
   # Allow username to be used as authentication artifact
-  def self.find_first_by_auth_conditions(warden_conditions)
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value", { :value => login.downcase }]).first
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
     end
   end
 end
