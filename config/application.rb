0637956685176a0b2f65449922aac4f3960c74b4
Giovanni Sakti <giosakti@gmail.com>
2018-03-24 17:58:36 +0700

    [gio] setup gouge library

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require 'gouge/core_ext/boolean_typecast'
 
 # Enable hirb in console
 Class.new Rails::Railtie do
