d7b5ee426e3f576330b469df9cb40927c04b5413
Giovanni Sakti <giosakti@gmail.com>
2018-03-24 19:29:19 +0700

    [gio] add convenience routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    post 'logout', to: 'devise/sessions#destroy'
+  end
 end
