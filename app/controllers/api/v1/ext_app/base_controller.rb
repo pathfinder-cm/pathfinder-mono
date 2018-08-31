4c0bda66152206554e7d5f131277a92ac82a67a5
Giovanni Sakti <giosakti@gmail.com>
2018-08-31 07:04:14 +0700

    [gio] update class reference

--- app/controllers/api/v1/ext_app/base_controller.rb
+++ app/controllers/api/v1/ext_app/base_controller.rb
@@ -2,7 +2,7 @@ class ::Api::V1::ExtApp::BaseController < ::Api::BaseController
   before_action :authenticate_by_access_token!
 
   def authenticate_by_access_token!
-    unless ExtApp.valid_access_token?(fetch_access_token)
+    unless ::ExtApp.valid_access_token?(fetch_access_token)
       raise ::UnauthorizedException
     end
   end
