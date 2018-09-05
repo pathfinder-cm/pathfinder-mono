6fbc39475e4a08c05aa7ddc17eb4e7facabfcfd7
opanmustopah <opan.mustopah@go-jek.com>
2018-09-05 12:33:59 +0700

    [opan/tara] Add new env variables Add feature to scheduler based on nodes ram

--- lib/container_scheduler.rb
+++ lib/container_scheduler.rb
@@ -6,27 +6,48 @@ class ContainerScheduler
     loop do
       p 'Scheduling Container...'
 
-      counter = 0
+      @counter = 0
       Cluster.all.each do |cluster|
         containers = cluster.containers.
           where(status: 'PENDING').
           order(last_status_update_at: :asc)
 
-        containers.each do |container|
-          node = Node.
-            select('nodes.id, nodes.hostname, COUNT(containers) AS containers_count').
-            joins('LEFT OUTER JOIN containers ON nodes.id = containers.node_id AND containers.status IN (\'SCHEDULED\', \'PROVISIONED\')').
-            where('nodes.cluster_id = ?', cluster.id).
-            group('nodes.id, nodes.hostname').
-            order('containers_count ASC').
-            first
-          schedule_container!(container, node)
-          counter += 1
+        if ENV['NODE_SCHEDULING_TYPE'] == 'CONTAINER'
+          find_based_on_container(containers, cluster)
+        else
+          find_based_on_ram(containers, cluster)
         end
       end
 
-      p "#{counter} container(s) scheduled."
+      p "#{@counter} container(s) scheduled."
       sleep(5.seconds)
+    end
+  end
+
+  def find_based_on_container(containers, cluster)
+    containers.each do |container|
+      node = Node.
+        select('nodes.id, nodes.hostname, COUNT(containers) AS containers_count').
+        joins('LEFT OUTER JOIN containers ON nodes.id = containers.node_id AND containers.status IN (\'SCHEDULED\', \'PROVISIONED\')').
+        where('nodes.cluster_id = ?', cluster.id).
+        group('nodes.id, nodes.hostname').
+        order('containers_count ASC').
+        first
+      schedule_container!(container, node)
+      @counter += 1
+    end
+  end
+
+  def find_based_on_ram(containers, cluster)
+    node_ram_treshold = ENV['NODE_RAM_TRESHOLD'] || 80
+    containers.each do |container|
+      node = Node.
+        select('nodes.id, nodes.hostname, ceil((nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) * 100) AS mem_used_percentage').
+        where('nodes.cluster_id = ? AND ceil((nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) * 100) <= ?', cluster.id, node_ram_treshold).
+        order('mem_used_percentage').
+        first
+      schedule_container!(container, node)
+      @counter += 1
     end
   end
 
