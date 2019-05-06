36a99a7f61674afca7cbb4be64e0f735994fa068
Tara Baskara <tara.baskara@gmail.com>
2019-05-06 15:05:57 +0700

    [tara/puja] modified spec: renaming image to image_alias

--- spec/factories/containers.rb
+++ spec/factories/containers.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     association :cluster
     sequence(:hostname) {|n| "cluster-#{n}" }
     sequence(:ipaddress) {|n| "10.0.0.#{n}" }
-    image { 'linux' }
+    image_alias { 'linux' }
   end
 end
