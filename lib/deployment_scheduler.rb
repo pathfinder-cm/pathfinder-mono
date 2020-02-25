d583e06ed7bff1763fcfa7b02a2178f4cb1bf161
Galuh Estya <galuhestya@gmail.com>
2020-02-25 16:07:52 +0700

    deployment scheduler should create new container based on the count in deployment
    Co-authored-by: Galuh Estya <galuhestya@gmail.com>

--- lib/deployment_scheduler.rb
+++ lib/deployment_scheduler.rb
@@ -1,2 +1,13 @@
 class DeploymentScheduler
+  def schedule_single(deployment)
+    deployment.container_names.each do |container_name|
+      Container.create(
+          cluster_id: deployment.cluster_id,
+          hostname: container_name,
+          source: deployment.bootstrapper[:source],
+          image_alias: deployment.bootstrapper[:image],
+          bootstrappers: deployment.bootstrapper[:bootstrapper]
+        )
+    end
+  end
 end
--- spec/lib/deployment_scheduler_spec.rb
+++ spec/lib/deployment_scheduler_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe DeploymentScheduler do
+  let(:deployment_scheduler) { DeploymentScheduler.new }
+
+  describe "#schedule_single" do
+    context "container doesn't exists" do
+      it "create new container" do
+        deployment = create(:deployment, name: 'hitsu-consul', count: rand(1..99))
+        deployment_scheduler.schedule_single(deployment)
+
+        expect(Container.pluck(:hostname)).to include(*deployment.container_names)
+      end
+    end
+  end
 end
