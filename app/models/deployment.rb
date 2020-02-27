2fc7a0c58eefd9738c952f2bc6d3bd76858b8f1c
Daniel Pintara <nieltansah@gmail.com>
2020-02-27 16:18:55 +0700

    models/deployment: Expose managed containers

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -10,4 +10,13 @@ class Deployment < ApplicationRecord
   def container_names
     (0..count-1).map{ |i| "#{name}-%02d" % (i+1) }
   end
+
+  def managed_containers
+    Container.where("hostname ~* ?", managed_containers_hostname_regexp)
+  end
+
+  private
+  def managed_containers_hostname_regexp
+    "^#{Regexp.escape(name)}-[0-9]{2}$"
+  end
 end
--- spec/models/deployment_spec.rb
+++ spec/models/deployment_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Deployment, type: :model do
-  let(:deployment) { build_stubbed(:deployment) }
-
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
@@ -13,6 +11,8 @@ RSpec.describe Deployment, type: :model do
   end
 
   describe "methods" do
+    let(:deployment) { create(:deployment) }
+
     describe "#container_names" do
       it "returns empty list if count is 0" do
         deployment.count = 0
@@ -22,6 +22,18 @@ RSpec.describe Deployment, type: :model do
       it "returns list of container names" do
         deployment.count = rand(1..99)
         expect(deployment.container_names).to match_array (1..deployment.count).map{ |i| "#{deployment.name}-%02d" % i }
+      end
+    end
+
+    describe "#managed_containers" do
+      let(:cluster) { deployment.cluster }
+
+      it "returns list of managed existing containers" do
+        containers = [
+          Container.create(cluster: cluster, hostname: "#{deployment.name}-01"),
+          Container.create(cluster: cluster, hostname: "#{deployment.name}-02"),
+        ]
+        expect(deployment.managed_containers).to match_array(containers)
       end
     end
   end
