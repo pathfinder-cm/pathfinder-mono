151c76c075add651e912cbe8db1a1a90b550b8e0
Daniel Pintara <nieltansah@gmail.com>
2020-02-27 12:08:32 +0700

    lib/deployment_scheduler: Clean up and add test of deleted container

--- spec/lib/deployment_scheduler_spec.rb
+++ spec/lib/deployment_scheduler_spec.rb
@@ -5,36 +5,53 @@ RSpec.describe DeploymentScheduler do
   let(:cluster) {create(:cluster)}
 
   describe "#schedule" do
-    it "iterates deployments and call schedule_single" do
-      n_deployment = rand(1..10)
-      deployments = (1..n_deployment).map do
-        create(:deployment, cluster: cluster, count: rand(1..3))
+    it "processes all deployments" do
+      deployments = (1..3).map do
+        create(:deployment, cluster: cluster, count: 3)
       end
 
       deployment_scheduler.schedule
       hostnames = Container.where(cluster: cluster).pluck(:hostname)
       expect(hostnames).to include(*deployments.flat_map {
-        |deployment| deployment.container_names }
-      )
+        |deployment| deployment.container_names
+      })
     end
   end
 
   describe "#schedule_single" do
-    context "container doesn't exists" do
-      before(:each) do
-        @deployment = create(:deployment, name: 'hitsu-consul', count: rand(1..99))
-        deployment_scheduler.schedule_single(@deployment)
+    context "create operation" do
+      context "no existing container" do
+        before(:each) do
+          @deployment = create(:deployment, name: 'hitsu-consul', count: 3)
+          deployment_scheduler.schedule_single(@deployment)
+        end
+
+        it "creates containers" do
+          hostnames = Container.where(cluster: @deployment.cluster).pluck(:hostname)
+          expect(hostnames).to include(*@deployment.container_names)
+        end
+
+        it "creates containers with source" do
+          container = Container.find_by(
+            cluster: @deployment.cluster, hostname: @deployment.container_names.first)
+          expect(container.source).not_to eq(nil)
+        end
       end
 
-      it "creates containers" do
-        hostnames = Container.where(cluster: @deployment.cluster).pluck(:hostname)
-        expect(hostnames).to include(*@deployment.container_names)
-      end
+      context "deleted container" do
+        before(:each) do
+          container = create(:container, cluster: cluster, hostname: 'hitsu-consul-01')
+          container.status = Container.statuses[:deleted]
+          container.save!
 
-      it "created containers have sources" do
-        container = Container.find_by(
-          cluster: @deployment.cluster, hostname: @deployment.container_names.first)
-        expect(container.source).not_to eq(nil)
+          deployment = create(:deployment, cluster: cluster, name: 'hitsu-consul', count: 2)
+          deployment_scheduler.schedule_single(deployment)
+        end
+
+        it "keeps creating containers" do
+          containers = Container.where(cluster: cluster, hostname: 'hitsu-consul-01')
+          expect(containers.count).to eq(2)
+        end
       end
     end
 
