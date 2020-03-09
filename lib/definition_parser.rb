2f9559614529d3596a6b9aa1574e3a506ad09d58
Daniel Pintara <nieltansah@gmail.com>
2020-03-09 15:16:40 +0700

    lib/definition_parser: Use MethodCatalog for callable methods

--- lib/definition_parser.rb
+++ lib/definition_parser.rb
@@ -15,16 +15,31 @@ class DefinitionParser
   end
 
   private
+  module MethodCatalog
+    extend self
+
+    def passthrough(value: )
+      value
+    end
+  end
+
   def parse_query_text(text)
     return nil unless text.start_with? KEYWORD
     text[KEYWORD.length..-1]
   end
 
   def handle_query_text(query)
-    uri = URI(query)
-    raise ArgumentError, "Unknown $pf-meta function name: #{uri.path}" if uri.path != "passthrough"
+    handle_query_uri URI(query)
+  end
 
-    URI.decode_www_form(uri.query).to_h['value']
+  def handle_query_uri(uri)
+    method_name = uri.path.to_sym
+    method_params = URI.decode_www_form(uri.query).to_h.symbolize_keys
+
+    unless MethodCatalog.public_methods.include? method_name
+      raise ArgumentError, "Unknown $pf-meta function name: #{method_name}"
+    end
+    MethodCatalog.public_send method_name, **method_params
   end
 
   def handle_text(text)
