6bc0c4a6972482975a97a4e4d017ff984f28bbbd
Daniel Pintara <nieltansah@gmail.com>
2020-03-11 10:03:27 +0700

    lib/deployment_scheduler: Fix tests by providing context

--- spec/lib/definition_parser_spec.rb
+++ spec/lib/definition_parser_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe DefinitionParser do
+  let(:deployment) { create(:deployment) }
+  let(:context) { DefinitionContext.new(deployment, "#{deployment.name}-01") }
   let(:parser) { DefinitionParser.new }
 
   describe "#parse" do
     it "returns same object of object with no special keyword" do
       input = {}
-      expect(parser.parse(input)).to eq(input)
+      expect(parser.parse(context, input)).to eq(input)
     end
 
     it "raises error if function name is unknown" do
       input = "$pf-meta:unknown?value=text"
 
-      expect { parser.parse(input) }.to raise_error(
+      expect { parser.parse(context, input) }.to raise_error(
         ArgumentError, "Unknown $pf-meta function name: unknown"
       )
     end
@@ -25,13 +27,13 @@ RSpec.describe DefinitionParser do
         output = {
           "text": "text"
         }
-        expect(parser.parse(input)).to eq(output)
+        expect(parser.parse(context, input)).to eq(output)
       end
 
       it "handles function in Array" do
         input = ["$pf-meta:passthrough?value=text"]
         output = ["text"]
-        expect(parser.parse(input)).to eq(output)
+        expect(parser.parse(context, input)).to eq(output)
       end
     end
 
@@ -49,7 +51,7 @@ RSpec.describe DefinitionParser do
       it "returns array of deployment-managed IP addresses" do
         input = "$pf-meta:deployment_ip_addresses?deployment_name=hitsu-consul"
         output = ['10.0.0.2', '10.0.0.3']
-        expect(parser.parse(input)).to eq(output)
+        expect(parser.parse(context, input)).to eq(output)
       end
     end
   end
