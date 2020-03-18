2e83ca1f11542ec09e8b0901a106e07b06a0004b
Galuh Estya <galuhestya@gmail.com>
2020-03-18 11:29:00 +0700

    deployment serializer should use container serializer instead

--- app/serializers/api/v2/ext_app/deployment_serializer.rb
+++ app/serializers/api/v2/ext_app/deployment_serializer.rb
@@ -15,31 +15,7 @@ class ::Api::V2::ExtApp::DeploymentSerializer < ::Api::V2::BaseSerializer
       name: deployment.name,
       count: deployment.count,
       definition: deployment.definition,
-      containers: deployment.managed_containers.map{|container| serialize_container(container)}
-    }
-  end
-
-  def serialize_container(container)
-    {
-      hostname: container.hostname,
-      ipaddress: container.ipaddress,
-      source: {
-        id: container.source&.id,
-        source_type: container.source&.source_type,
-        mode: container.source&.mode,
-        remote: {
-          id: container.source&.remote&.id,
-          name: container.source&.remote&.name,
-          server: container.source&.remote&.server,
-          protocol: container.source&.remote&.protocol,
-          auth_type: container.source&.remote&.auth_type
-        },
-        fingerprint: container.source&.fingerprint,
-        alias: container.source&.alias
-      },
-      bootstrappers: container.bootstrappers,
-      status: container.status,
-      last_status_update_at: container.last_status_update_at,
+      containers: deployment.managed_containers.map{|container| ::Api::V2::ExtApp::ContainerSerializer.new(container).to_h[:data]}
     }
   end
 end
