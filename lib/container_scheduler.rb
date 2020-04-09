44fe69fdaa65c07de212e0e012f5daea527d4ecf
Daniel Pintara <nieltansah@gmail.com>
2020-04-09 13:32:30 +0700

    Allocate node to containers

--- lib/container_scheduler.rb
+++ lib/container_scheduler.rb
@@ -1,76 +1,31 @@
 class ContainerScheduler
   def initialize
+    @weight_stateful = ENV['SCHEDULER_STATEFUL_WEIGHT'] || 0
+    @weight_mem_free_mb = ENV['SCHEDULER_MEM_FREE_MB_WEIGHT'] || 0
+    @weight_mem_free_ratio = ENV['SCHEDULER_MEM_FREE_RATIO_WEIGHT'] || 0
   end
 
   def schedule
-    if ENV['SCHEDULER_TYPE'] == 'MEMORY'
-      p 'Scheduling container based on node memory'
-    else
-      p 'Scheduling container based on node container numbers'
+    n_containers = 0
+    Container.all.find_each do |container|
+      schedule_container(container)
+      n_containers += 1
     end
 
-    counter = 0
-    Cluster.all.each { |cluster| counter += process_cluster(cluster) }
-
-    p "#{counter} container(s) scheduled."
+    p "#{n_containers} container(s) scheduled."
   end
 
-  def process_cluster(cluster)
-    pending_containers = cluster.containers.pending
+  def schedule_container(container)
+    node = find_best_node(container)
+    return if node.nil?
 
-    counter = 0
-    pending_containers.each do |container|
-      if ENV['SCHEDULER_TYPE'] == 'MEMORY'
-        node = find_based_on_memory(cluster)
-      else
-        node = find_based_on_container_num(cluster)
-      end
-
-      unless node.nil?
-        schedule_container!(container, node)
-        counter += 1
-      end
-    end
-
-    return counter
-  end
-
-  def schedule_container!(container, node)
     ActiveRecord::Base.transaction do
-      p "Schedule container #{container.hostname} in #{node.hostname}"
       container.update_attribute(:node_id, node.id)
-      container.update_status('SCHEDULED')
+      container.update_status(Container.statuses[:scheduled])
     end
   end
 
-  private
-    def find_based_on_container_num(cluster)
-      Node.
-        select('nodes.id, nodes.hostname, COUNT(containers) AS containers_count').
-        joins('LEFT OUTER JOIN containers ON nodes.id = containers.node_id AND containers.status IN (\'SCHEDULED\', \'PROVISIONED\')').
-        where('nodes.cluster_id = ?', cluster.id).
-        group('nodes.id, nodes.hostname').
-        order('containers_count ASC').
-        first
-    end
-
-    def find_based_on_memory(cluster)
-      memory_threshold = ENV['SCHEDULER_MEMORY_THRESHOLD'] || 50
-      Node.
-        select('nodes.id'\
-          ', nodes.hostname'\
-          ', COUNT(containers) AS containers_count'\
-          ', ceil((nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) * 100) AS mem_used_percentage').
-        joins('LEFT OUTER JOIN containers'\
-          ' ON nodes.id = containers.node_id'\
-          ' AND containers.status IN (\'SCHEDULED\', \'PROVISIONED\')').
-        where('nodes.cluster_id = ?'\
-          ' AND ceil(CASE WHEN (nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) IS NULL'\
-          ' THEN 1'\
-          ' ELSE (nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) END * 100) <= ?',
-          cluster.id, memory_threshold).
-        group('nodes.id, nodes.hostname').
-        order('containers_count ASC, mem_used_percentage ASC').
-        first
-    end
+  def find_best_node(container)
+    Node.first
+  end
 end
--- spec/lib/container_scheduler_spec.rb
+++ spec/lib/container_scheduler_spec.rb
@@ -1,56 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe ContainerScheduler do
-  describe "#process_cluster" do
+  let(:scheduler) { ContainerScheduler.new }
+
+  context "validation" do
+    let!(:node) { create(:node) }
+    let!(:container_1) { create(:container, cluster: node.cluster) }
+    let!(:container_2) { create(:container, cluster: node.cluster) }
+    let!(:container_3) { create(:container) }
+
     before(:each) do
-      @cluster = create(:cluster)
+      container_2.update_status(Container.statuses[:bootstrapped])
+      scheduler.schedule
+
+      container_1.reload
+      container_2.reload
+      container_3.reload
     end
 
-    context "Scheduler based on Memory" do
-      before(:each) do
-        ENV['SCHEDULER_TYPE'] = 'MEMORY'
-        @node_1 = create(:node, cluster: @cluster, mem_used_mb: 10, mem_total_mb: 100)
-        @node_2 = create(:node, cluster: @cluster, mem_used_mb: 90, mem_total_mb: 100)
-        @node_3 = create(:node, cluster: @cluster, mem_used_mb: 5, mem_total_mb: 100)
+    describe "container_1 (pending)" do
+      it "is allocated to node" do
+        expect(container_1.node).to eq(node)
       end
 
-      it "should properly schedule containers to correct node" do
-        c1 = create(:container, cluster: @cluster)
-        c2 = create(:container, cluster: @cluster)
-        c3 = create(:container, cluster: @cluster)
-        ContainerScheduler.new.process_cluster(@cluster)
-        c1.reload
-        c2.reload
-        c3.reload
-        expect(c1.node).to eq @node_3
-        expect(c2.node).to eq @node_1
-        expect(c3.node).to eq @node_3
-      end
-    end
-
-    context "Scheduler based on Container Number" do
-      before(:each) do
-        ENV['SCHEDULER_TYPE'] = 'CONTAINER_NUM'
-        @node_1 = create(:node, cluster: @cluster)
-        @node_2 = create(:node, cluster: @cluster)
-        container = create(:container, cluster: @cluster, node: @node_2)
-        container.update_status("SCHEDULED")
-        container = create(:container, cluster: @cluster, node: @node_2)
-        container.update_status("SCHEDULED")
-        @node_3 = create(:node, cluster: @cluster)
-      end
-
-      it "should properly schedule containers to correct node" do
-        c1 = create(:container, cluster: @cluster)
-        c2 = create(:container, cluster: @cluster)
-        c3 = create(:container, cluster: @cluster)
-        ContainerScheduler.new.process_cluster(@cluster)
-        c1.reload
-        c2.reload
-        c3.reload
-        expect(c1.node).to eq @node_1
-        expect(c2.node).to eq @node_3
-        expect(c3.node).to eq @node_1
+      it "has SCHEDULED status" do
+        expect(container_1.status).to eq(Container.statuses[:scheduled])
       end
     end
   end
