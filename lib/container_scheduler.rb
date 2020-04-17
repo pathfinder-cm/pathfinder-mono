119b10ae9c3044465d85c8e8395809f6430d14cb
Daniel Pintara <nieltansah@gmail.com>
2020-04-17 11:46:17 +0700

    Refactor container scheduler so it's easier to add more limits

--- lib/container_scheduler.rb
+++ lib/container_scheduler.rb
@@ -15,6 +15,7 @@ class ContainerScheduler
     Rails.logger.info "#{n_containers} container(s) scheduled."
   end
 
+  private
   def schedule_container(container)
     node_id = find_best_node_id(container)
     return if node_id.nil?
@@ -31,19 +32,31 @@ class ContainerScheduler
       joins("LEFT OUTER JOIN containers ON nodes.id = containers.node_id AND containers.status NOT IN ('SCHEDULE_DELETION', 'DELETED')").
       group("nodes.id")
 
-    nodes = nodes.where(
-      "ceil(CASE WHEN (nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) IS NULL " \
-      "THEN 1 " \
-      "ELSE (nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) END * 100) <= ?",
-      @limit_mem_threshold
-    ) unless @limit_mem_threshold.nil?
-    nodes = nodes.having("COUNT(containers) <= ?", @limit_n_containers) unless @limit_n_containers.nil?
-    nodes = nodes.having(
-      "COUNT(containers) FILTER (WHERE container_type = 'STATEFUL') <= ?", @limit_n_stateful_containers
-    ) unless @limit_n_stateful_containers.nil?
+    nodes = limit_by_mem_threshold(nodes, @limit_mem_threshold) unless @limit_mem_threshold.nil?
+    nodes = limit_by_n_containers(nodes, @limit_n_containers) unless @limit_n_containers.nil?
+    nodes = limit_by_n_stateful_containers(nodes, @limit_n_stateful_containers) unless @limit_n_stateful_containers.nil?
 
     node_ids = nodes.pluck(:id)
     Rails.logger.info "Selecting a node from #{node_ids.length} candidate node(s)"
     node_ids.sample
+  end
+
+  def limit_by_mem_threshold(nodes, mem_threshold)
+    nodes.where(
+      "ceil(CASE WHEN (nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) IS NULL " \
+      "THEN 1 " \
+      "ELSE (nodes.mem_used_mb::decimal/nodes.mem_total_mb::decimal) END * 100) <= ?",
+      mem_threshold
+    )
+  end
+
+  def limit_by_n_containers(nodes, n_containers)
+    nodes.having("COUNT(containers) <= ?", n_containers)
+  end
+
+  def limit_by_n_stateful_containers(nodes, n_stateful_containers)
+    nodes.having(
+      "COUNT(containers) FILTER (WHERE container_type = 'STATEFUL') <= ?", n_stateful_containers
+    )
   end
 end
